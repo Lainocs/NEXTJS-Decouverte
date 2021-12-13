8fc6e2b874a4febdb7e2f0f80e0245eb36868fd6
Nicolas de Garrigues <ndegarrigues@gmail.com>
2021-12-13 16:33:39 +0100

    :wrench: UPDATE cards content

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/Card.module.css";
 
 export default function Card(props){
+    const [open, setOpen] = useState(false);
     return (
         <div className={styles.card}>
             <h3>{props.title} </h3>
             <Image src={props.image} width={200} height={200} alt={props.image} />
             <p> Titre phare : {props.description} </p>
             <p> Price : {props.price} </p>
-            <button>Acheter</button>
+            <button onClick={() => open = true}>Acheter</button>
         </div>
+        
     )
 }
